6ec37b1e8f06d3d868900fe80e0daad3af6ded99
Evelina Norlin <evelina.norlin@hotmail.com>
2022-12-09 10:28:24 +0100

    lagt till todo i main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import './style/style.scss';
+
+// TODO
+// - MENY -> ska förvinna när man scrollar ner och
+// synas när man skrollar upp. mobil-menyn ska ploppa upp när man trycker
+// på burgern. samt lägga in animationer när menyer försvinner/dyker upp
+// - RUBRIKER -> ska tonasin när de kommer i bild. Kanske en fräck animation i header-texten
+// -UPP-KNAPP -> knapp som åker med ner som går att trycka på för att åka upp
+// -POP-UP -> pop-up ruta som frågar om man vill signa upp på nyhetsbrev
+// -EVENTS -> lägga in dessa som objekt i en array. med en läs mer-knapp som öppnar
+// en pop-up med mer info om event
+// OMDÖMEN -> En slider med omdömen som går att bläddra i son går i en "cirkel"
+// SPRÅK -> knapp för att byta språk?
+// KONTAKT -> lägga in regex, ska synas vilka fält som är obligatoriska.
+// Går det att göra formuläret fungerande?
