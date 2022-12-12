b609596a10625f46516cb6351ad3e17f41d15d1a
Evelina Norlin <evelina.norlin@hotmail.com>
2022-12-12 13:47:12 +0100

    Lagt till objekt som går att öppna, ej stänga

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ const events = [
     startTime: '11:00',
     finishTime: '14:00',
     shortDescription: 'A workshop where we explore our biological intuition.',
-    longDescription: '<p>Welcome to an afternoon of exploratory self-leadership, in the sign of movement and yoga. </p> In our fast-moving world, we make thousands of decisions every day, the brain spins faster than ever and is often involved in most of the decisions you make. But sometimes you get a gnawing feeling in your stomach that might mean something isnt right. Do you recognize yourself?</p><p>The brain, heart and stomach are connected by complex nervous systems and are constantly working together, but often the heart and stomach are silenced and the brain takes command.</p><p>In this workshop you will have the opportunity to dive deeper into the body to explore your other two "brains", the heart and the stomach. With the help of meditation, free movement exploration, yoga and reflection, you get the chance to refine your ability to listen to the bodys signals, which are always there and want to be heard.</p><p>During our hours together we will meditate, move, relax and reflect. After the session, there will be tea, snacks and the opportunity to socialize, or hang out as we say in Gothenburg.</p>',
+    longDescription: '<p>Welcome to an afternoon of exploratory self-leadership, in the sign of movement and yoga. </p> <p> In our fast-moving world, we make thousands of decisions every day, the brain spins faster than ever and is often involved in most of the decisions you make. But sometimes you get a gnawing feeling in your stomach that might mean something isnt right. Do you recognize yourself?</p><p>The brain, heart and stomach are connected by complex nervous systems and are constantly working together, but often the heart and stomach are silenced and the brain takes command.</p><p>In this workshop you will have the opportunity to dive deeper into the body to explore your other two "brains", the heart and the stomach. With the help of meditation, free movement exploration, yoga and reflection, you get the chance to refine your ability to listen to the bodys signals, which are always there and want to be heard.</p><p>During our hours together we will meditate, move, relax and reflect. After the session, there will be tea, snacks and the opportunity to socialize, or hang out as we say in Gothenburg.</p>',
     img: 'explorative.png',
   },
   {
@@ -44,7 +44,7 @@ const events = [
     startTime: '12:00',
     finishTime: '17:00',
     shortDescription: 'A workshop where we explore our fascia',
-    longDescription: '<p>Welcome to an afternoon of exploratory self-leadership, in the sign of movement and yoga. </p> In our fast-moving world, we make thousands of decisions every day, the brain spins faster than ever and is often involved in most of the decisions you make. But sometimes you get a gnawing feeling in your stomach that might mean something isnt right. Do you recognize yourself?</p><p>The brain, heart and stomach are connected by complex nervous systems and are constantly working together, but often the heart and stomach are silenced and the brain takes command.</p><p>In this workshop you will have the opportunity to dive deeper into the body to explore your other two "brains", the heart and the stomach. With the help of meditation, free movement exploration, yoga and reflection, you get the chance to refine your ability to listen to the bodys signals, which are always there and want to be heard.</p><p>During our hours together we will meditate, move, relax and reflect. After the session, there will be tea, snacks and the opportunity to socialize, or hang out as we say in Gothenburg.</p>',
+    longDescription: '<p>Welcome to an afternoon of exploratory self-leadership, in the sign of movement and yoga. </p> <p> In our fast-moving world, we make thousands of decisions every day, the brain spins faster than ever and is often involved in most of the decisions you make. But sometimes you get a gnawing feeling in your stomach that might mean something isnt right. Do you recognize yourself?</p><p>The brain, heart and stomach are connected by complex nervous systems and are constantly working together, but often the heart and stomach are silenced and the brain takes command.</p><p>In this workshop you will have the opportunity to dive deeper into the body to explore your other two "brains", the heart and the stomach. With the help of meditation, free movement exploration, yoga and reflection, you get the chance to refine your ability to listen to the bodys signals, which are always there and want to be heard.</p><p>During our hours together we will meditate, move, relax and reflect. After the session, there will be tea, snacks and the opportunity to socialize, or hang out as we say in Gothenburg.</p>',
     img: 'fascia.png',
   },
   {
@@ -52,15 +52,15 @@ const events = [
     name: 'Embodied flow classes',
     year: 2023,
     month: 5,
-    date: [1, 8, 15],
+    date: 1,
     location: 'Fysiken Yoga',
     startTime: '19:00',
     finishTime: '20:30',
     shortDescription: 'A workshop where we explore our fascia',
-    longDescription: '<p>Welcome to an afternoon of exploratory self-leadership, in the sign of movement and yoga. </p> In our fast-moving world, we make thousands of decisions every day, the brain spins faster than ever and is often involved in most of the decisions you make. But sometimes you get a gnawing feeling in your stomach that might mean something isnt right. Do you recognize yourself?</p><p>The brain, heart and stomach are connected by complex nervous systems and are constantly working together, but often the heart and stomach are silenced and the brain takes command.</p><p>In this workshop you will have the opportunity to dive deeper into the body to explore your other two "brains", the heart and the stomach. With the help of meditation, free movement exploration, yoga and reflection, you get the chance to refine your ability to listen to the bodys signals, which are always there and want to be heard.</p><p>During our hours together we will meditate, move, relax and reflect. After the session, there will be tea, snacks and the opportunity to socialize, or hang out as we say in Gothenburg.</p>',
+    longDescription: '<p>Welcome to an afternoon of exploratory self-leadership, in the sign of movement and yoga. </p> <p>In our fast-moving world, we make thousands of decisions every day, the brain spins faster than ever and is often involved in most of the decisions you make. But sometimes you get a gnawing feeling in your stomach that might mean something isnt right. Do you recognize yourself?</p><p>The brain, heart and stomach are connected by complex nervous systems and are constantly working together, but often the heart and stomach are silenced and the brain takes command.</p><p>In this workshop you will have the opportunity to dive deeper into the body to explore your other two "brains", the heart and the stomach. With the help of meditation, free movement exploration, yoga and reflection, you get the chance to refine your ability to listen to the bodys signals, which are always there and want to be heard.</p><p>During our hours together we will meditate, move, relax and reflect. After the session, there will be tea, snacks and the opportunity to socialize, or hang out as we say in Gothenburg.</p>',
     img: 'embodied_flow.png',
   },
-]
+];
 
 // Kod för menyn, fadear in och ut vid scroll
 
@@ -79,23 +79,48 @@ window.addEventListener('scroll', () => {
 // Skapa event-cards
 
 const eventsHolder = document.querySelector('#events-holder') as HTMLHtmlElement;
+let eventsHtml = '';
 
 // kolla på constructors, kanske går att använda här?
 
-function createEvents() {
-  for (let i = 0; i < events.length; i++) {
-    console.log(events[i].name);
-    // OBS lägg til alt-text på bilder i objekt
-    eventsHolder.innerHTML += `
-      <div>    
-        <img src='./public/images/${events[i].img}'>
-        <h3>${events[i].name}</h3>
-        <p>When? ${events[i].date}-${events[i].month}-${events[i].year}</p>
-        <p>Where? ${events[i].location}</p>
-        <p>${events[i].shortDescription}</p>
-        <button id="event-btn">Read more and sign up</button>
-      </div>`;
-  }
-}
+events.forEach((event) => {
+  eventsHtml += `
+     <div class="event-card">    
+       <img src='./public/images/${event.img}'>
+       <div class="event-description">
+          <h3>${event.name}</h3>
+          <p><span>When?</span><br> ${event.date}-${event.month}-${event.year}</p>
+          <p><span>Where?</span><br> ${event.location}</p>
+          <p>${event.shortDescription}</p>
+       </div>
+       <button id="${event.id}">Read more</button>
+       <div class="event-popup" id="event-popup">
+          <div class="event-long-description">
+            <button class="close-btn" id="${event.id}">X</button>
+            <h2>${event.name}</h2>
+            <img src='./public/images/${event.img}'>
+            <h3>${event.date}-${event.month}-${event.year}, ${event.startTime}-${event.finishTime}  ||  
+            ${event.location} </h3>
+            <p>${event.longDescription}</p>
+            <button>Sign up</button>
+          </div>
+       </div>
+     </div>`;
+});
 
-createEvents();
+eventsHolder.innerHTML = eventsHtml;
+
+// const closeBtn = eventsHolder.querySelector('#close-btn') as HTMLElement;
+// const eventPopup = eventsHolder.querySelector('#event-popup') as HTMLElement;
+// console.log(eventPopup)
+
+// visar en lång beskrivning av eventet vid klick på knappen
+eventsHolder.addEventListener('click', (e) => {
+  const currentEventPopup = document.getElementById(e.target.id).parentElement.lastElementChild as HTMLElement;
+  currentEventPopup.style.display = 'block';
+});
+
+// // stänger den långa beskrivningen när man trycker på X
+// closeBtn.addEventListener('click', (e) => {
+//   eventPopup.style.display = 'none';
+// });
